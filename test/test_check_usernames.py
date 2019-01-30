01a713815e5cd58aa5ffc6e1e58abb82701d4fee
Abhinav Kasamsetty <abhinavkasamsetty@gmail.com>
2019-01-29 23:17:48 -0600

    Put vcr cassettes in fixtures folder

--- test/test_check_usernames.py
+++ test/test_check_usernames.py
@@ -3,14 +3,14 @@ import vcr
 
 from src.check_usernames import check
 
-@vcr.use_cassette('test/vcr_cassettes/check_usernames/check-username-success.yaml')
+@vcr.use_cassette('test/fixtures/vcr_cassettes/check_usernames/check-username-success.yaml')
 def test_check_username_success(capfd):
     check("Congress")
     out, _ = capfd.readouterr()
     assert out == "Congress\n"
 
 
-@vcr.use_cassette('test/vcr_cassettes/check_usernames/check-username-failure.yaml')
+@vcr.use_cassette('test/fixtures/vcr_cassettes/check_usernames/check-username-failure.yaml')
 def test_check_username_failure(capfd):
     check("Azertytreza2402")
     out, _ = capfd.readouterr()
