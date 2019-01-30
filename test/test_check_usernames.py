0176258632f498bff0ca8b2558b1e2f6a281dd7c
Abhinav Kasamsetty <abhinavkasamsetty@gmail.com>
2019-01-29 23:13:24 -0600

    Named vcr cassettes better and committed them

--- test/test_check_usernames.py
+++ test/test_check_usernames.py
@@ -3,15 +3,15 @@ import vcr
 
 from src.check_usernames import check
 
-@vcr.use_cassette('test/vcr_cassettes/check-success.yaml')
-def test_check_success(capfd):
+@vcr.use_cassette('test/vcr_cassettes/check_usernames/check-username-success.yaml')
+def test_check_username_success(capfd):
     check("Congress")
-    out, err = capfd.readouterr()
+    out, _ = capfd.readouterr()
     assert out == "Congress\n"
 
 
-@vcr.use_cassette('test/vcr_cassettes/check-failure.yaml')
-def test_check_failure(capfd):
+@vcr.use_cassette('test/vcr_cassettes/check_usernames/check-username-failure.yaml')
+def test_check_username_failure(capfd):
     check("Azertytreza2402")
-    out, err = capfd.readouterr()
+    out, _ = capfd.readouterr()
     assert out == "Azertytreza2402\nusername doesn't exist anymore: Azertytreza2402\n"
