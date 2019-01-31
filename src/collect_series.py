973ab243b852cd417b4b2f671096a7d5de2a3d6c
Abhinav Kasamsetty <abhinavkasamsetty@gmail.com>
2019-01-30 20:49:26 -0600

    Used jikanpy to redo collecting series

--- src/collect_series.py
+++ src/collect_series.py
@@ -1,60 +1,68 @@
 #!/usr/bin/python3.7
 
-from clients import mfalncfm_main
-
-import funcs
-import re
-import requests
+from jikanpy import Jikan
 import configparser
 
 config = configparser.ConfigParser()
 
 
-def get_series(search_url):
-    """Retrieve a list of (anime_id, anime_title) from a MAL advanced search URL.
+def get_series(season_info):
+    """Retrieve a list of (anime_id, anime_title) from the season info.
 
     Arguments:
-        search_url: the MAL advanced search URL, with the value of the `show`
-            query parameter being `%i`
+        season_info: dict containing season info from Jikanpy
 
     Returns:
-        A list of tuples representing (anime_id, anime_title). For example:
+        A dict from anime_id to anime_title. For example:
 
-        ('25879', 'Working!!!')
+        {25879: 'Working!!!'}
     """
-    series = []
-    headers = dict(config['myanimelist.net request header'].items())
+    tv_anime = dict()
+    for anime in season_info["anime"]:
+        if anime["type"] == "TV":
+            tv_anime[anime["mal_id"]] = anime["title"]
+    return tv_anime
 
-    with mfalncfm_main.Client() as cursor:
-        cursor.execute(
-            'SELECT secret.`key`, secret.value FROM secret WHERE secret.context="myanimelist.net request header"')
 
-        for (key, value) in cursor:
-            headers[key] = value
+def output_series(series):
+    """Output series ids and titles in the format:
 
-    for show in [0, 20]:
-        r = requests.get(search_url.format(show), headers=headers)
-        res = r.text
-        series.extend(re.findall(r'"/anime/(\d+)/(.+?)"', res))
-    return list(set(series))
+    id_1 title_1
+    id_2 title_2
+    """
+    with open("series.txt", "w", encoding="utf8") as f:
+        f.writelines(f"{a_id} {a_title}\n" for a_id, a_title in sorted(series))
+
+
+def output_series_titles(titles):
+    """Output series titles sorted lexicographically in the format:
+
+    title_1
+    title_2
+    """
+    with open("series_sorted.txt", "w", encoding="utf8") as f:
+        f.writelines(f"{title}\n" for title in sorted(titles))
 
 
 def main():
     config.read('config.ini')
-    SERIES = get_series(config['myanimelist.net']['advanced search url'])
-    SERIES_TXT = open("series.txt", "w")
-    SERIES_SORTED_TXT = open("series_sorted.txt", "w")
 
-    for a_id, a_title in sorted(SERIES):
-        SERIES_TXT.write("%s %s\n" % (a_id, a_title.replace("_", " ")))
+    # Ensure season is lowercase string and year is integer
+    season = config['myanimelist.net']['season'].lower()
+    year = int(config['myanimelist.net']['year'])
 
-    for a_id, a_title in sorted(SERIES, key=lambda x: x[1]):
-        SERIES_SORTED_TXT.write("%s\n" % a_title.replace("_", " "))
+    jikan = Jikan()
+    # Get list of anime in the season
+    season_info = jikan.season(year=year, season=season)
+    assert season_info['season_name'].lower() == season
+    assert season_info['season_year'] == year
 
-    print(len(SERIES))
+    series_dict = get_series(season_info)
+    series = series_dict.items()
+    print(len(series))
 
-    SERIES_TXT.close()
-    SERIES_SORTED_TXT.close()
+    output_series(series)
+    output_series_titles(series_dict.values())
 
 
 if __name__ == "__main__":
