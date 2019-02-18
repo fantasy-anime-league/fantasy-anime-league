e750e2e894273f863f24f483df53b75b4fb91677
Wilfred Wong <keiclone@users.noreply.github.com>
2019-02-18 01:08:30 -0500

    expanded season test by just a bit

--- test/integration/test_season.py
+++ test/integration/test_season.py
@@ -6,6 +6,10 @@ def test_retrieve_teams():
     with session_scope(True) as session:
         query = session.query(Season).filter(Season.id == 1)
         season = query.one()
-        assert len(season.teams) == 2
-        assert sorted(season.teams, key=lambda team: team.id)[
-            0].name == 'kei-clone'
+        assert len(season.teams) == 3
+
+        sorted_teams = sorted(season.teams, key=lambda team: team.id)
+
+        assert sorted_teams[0].name == 'kei-clone'
+        assert sorted_teams[1].name == 'abhinavk99'
+        assert sorted_teams[2].name == 'Congress'
