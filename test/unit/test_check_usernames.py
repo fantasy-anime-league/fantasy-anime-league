58624b040356206db92082c7532c1e08bebbf477
Abhinav Kasamsetty <abhinavkasamsetty@gmail.com>
2019-02-20 22:00:23 -0600

    removed extraneous fixtures and updated vcr cassettes folder

--- test/unit/test_check_usernames.py
+++ test/unit/test_check_usernames.py
@@ -4,21 +4,21 @@ import vcr
 from fal.check_usernames import check
 
 
-@vcr.use_cassette('test/fixtures/vcr_cassettes/check_usernames/check-username-success.yaml')
+@vcr.use_cassette('test/unit/fixtures/vcr_cassettes/check_usernames/check-username-success.yaml')
 def test_check_username_success(capfd):
     check("Congress")
     out, _ = capfd.readouterr()
     assert out == "Congress\n"
 
 
-@vcr.use_cassette('test/fixtures/vcr_cassettes/check_usernames/check-username-failure.yaml')
+@vcr.use_cassette('test/unit/fixtures/vcr_cassettes/check_usernames/check-username-failure.yaml')
 def test_check_username_failure(capfd):
     check("Azertytreza2402")
     out, _ = capfd.readouterr()
     assert out == "Azertytreza2402\nusername doesn't exist anymore: Azertytreza2402\n"
 
 
-@vcr.use_cassette('test/fixtures/vcr_cassettes/check_usernames/caps-username-success.yaml')
+@vcr.use_cassette('test/unit/fixtures/vcr_cassettes/check_usernames/caps-username-success.yaml')
 def test_caps_username_success(capfd):
     check("alExThEriOt")
     out, _ = capfd.readouterr()
