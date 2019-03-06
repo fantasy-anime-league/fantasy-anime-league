377056bca784f55247f93f1fd608d109e99bd8e0
Wilfred Wong <keiclone@users.noreply.github.com>
2019-03-06 00:52:44 -0500

    actually add our new files

---
+++ fal/models/team_weekly_anime.py
@@ -0,0 +1,20 @@
+from fal.models import Base
+
+from sqlalchemy import Column, Integer, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
+
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from fal.models import Anime, Team
+
+
+class TeamWeeklyAnime(Base):
+    __tablename__ = "team_weekly_anime"
+
+    team_id = Column(Integer, ForeignKey("team.id"), primary_key=True)
+    team = relationship("Team", back_populates="team_weekly_anime")
+    anime_id = Column(Integer, ForeignKey("anime.id"), primary_key=True)
+    anime = relationship("Anime")
+    week = Column(Integer, primary_key=True)
+    ace = Column(Boolean, default=False)
+    bench = Column(Boolean, default=False)
