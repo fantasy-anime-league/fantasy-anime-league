700d6a4ef01ff669bf2094b66edba7eb8c4dd842
Wilfred Wong <keiclone@users.noreply.github.com>
2019-04-04 09:53:49 -0400

    update mypy, fix config mock in test_anime_stats_controller

--- test/unit/test_anime_stats_controller.py
+++ test/unit/test_anime_stats_controller.py
@@ -14,6 +14,7 @@ config.read("config.ini")
 
 vcrpath = config.get('vcr', 'path')
 
+
 @patch('fal.controllers.anime_stats.get_forum_posts')
 @patch('fal.controllers.anime_stats.config')
 @patch('fal.controllers.anime_stats.session_scope')
@@ -21,11 +22,12 @@ vcrpath = config.get('vcr', 'path')
 def test_populate_anime_weekly_stats(session_scope_mock, config_mock, get_forum_posts, session_scope, config_functor, season_factory, anime_factory):
     session_scope_mock.side_effect = session_scope
     config_function = config_functor(
-        sections=['season info', 'weekly info'],
+        sections=['season info', 'weekly info', 'jikanpy'],
         kv={
             'season': 'spring',
             'year': 2018,
-            'current-week': 0
+            'current-week': 0,
+            'request-interval': 0
         }
     )
     config_mock.getint.side_effect = config_function
