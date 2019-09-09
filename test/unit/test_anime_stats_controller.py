a7ff7f388141efcdc02b93551b4917dfdac1f771
Wilfred Wong <keiclone@users.noreply.github.com>
2019-09-09 00:59:11 -0400

    fix tests, refresh some cassettes

--- test/unit/test_anime_stats_controller.py
+++ test/unit/test_anime_stats_controller.py
@@ -20,14 +20,27 @@ vcrpath = config.get('vcr', 'path')
 @patch('fal.controllers.anime_stats.session_scope')
 @vcr.use_cassette(f"{vcrpath}/anime_stats/populate_anime_weekly_stats.yaml")
 def test_populate_anime_weekly_stats(session_scope_mock, config_mock, get_forum_posts, session_scope, config_functor, season_factory, anime_factory):
+    '''Currently only testing for week 4 to test dropped scores.
+        Feel free to parametrize this test to do other weeks as well
+        if we need to test for other scoring configs'''
+
     session_scope_mock.side_effect = session_scope
     config_function = config_functor(
-        sections=['season info', 'weekly info', 'jikanpy'],
+        sections=[
+            'season info',
+            'weekly info',
+            'jikanpy',
+            'scoring info',
+            'scoring.dropped',
+        ],
         kv={
             'season': 'spring',
             'year': 2018,
-            'current-week': 0,
-            'request-interval': 0
+            'current-week': 4,
+            'forum-posts-every-n-weeks': 2,
+            'forum-post-multiplier': 25,
+            'request-interval': 0,
+            '4': -4  # for 'scoring.dropped' only. others are not valid this week 4
         }
     )
     config_mock.getint.side_effect = config_function
@@ -47,27 +60,29 @@ def test_populate_anime_weekly_stats(session_scope_mock, config_mock, get_forum_
 
     assert len(stats) == 3
 
-    assert stats[0].watching == 66231
-    assert stats[0].completed == 493615
+    print(stats)
+    assert stats[0].watching == 71944
+    assert stats[0].completed == 526969
     assert stats[0].anime_id == 1
 
     assert stats[1].score == 7.96
-    assert stats[1].favorites == 14425
+    assert stats[1].favorites == 14683
     assert stats[1].anime_id == 849
 
-    assert stats[2].dropped == 15745
+    assert stats[2].dropped == 17066
     assert stats[2].forum_posts == 0
     assert stats[2].anime_id == 30276
 
 
 @patch('fal.controllers.anime_stats.config')
-@vcr.use_cassette(f"{config.get('vcr','path')}/anime_stats/get_forum_posts.yaml")
+@vcr.use_cassette(f"{vcrpath}/anime_stats/get_forum_posts.yaml")
 def test_get_forum_posts(config_mock, anime_factory, config_functor, session):
     config_mock.getint.side_effect = config_functor(
-        sections=['weekly info'],
+        sections=['weekly info', 'scoring info'],
         kv={
-            'current-week': 0
+            'current-week': 6,
+            'forum-posts-every-n-weeks': 2,
         }
     )
     one_punch_man = anime_factory(id=30276, name="One Punch Man")
-    assert anime_stats.get_forum_posts(one_punch_man) == 3939
+    assert anime_stats.get_forum_posts(one_punch_man) == 327 + 426
