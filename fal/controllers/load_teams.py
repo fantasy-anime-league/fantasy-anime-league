9e78fab83db6b7da6f937bd254753873347a48d4
Wilfred Wong <keiclone@users.noreply.github.com>
2019-11-10 01:02:58 -0500

    add assertion on load_teams to make sure anime season and team season are the same

--- fal/controllers/load_teams.py
+++ fal/controllers/load_teams.py
@@ -60,6 +60,7 @@ def add_anime_to_team(
     for anime_name in anime_lines:
         anime_name = anime_name.strip()
         anime = Anime.get_anime_from_database_by_name(anime_name, session)
+        assert anime.season_id == team.season_id
         if not anime:
             print(
                 f"{team.name} has {anime_name} on their team,"
